d3dcab61b973193fb889e89e539f13ba5185af41
Foxx05 <theocolpaert8@gmail.com>
2025-04-30 16:29:32 +0200

    Anim V1

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -8,3 +8,20 @@ function scrollToTop() {
 }
 
 backToTopBtn.addEventListener("click", scrollToTop);
+
+
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.utils.toArray('.animate-on-scroll').forEach(elem => {
+  gsap.from(elem, {
+    opacity: 0,
+    y: 100,
+    duration: 1,
+    ease: "power2.out",
+    scrollTrigger: {
+      trigger: elem,
+      start: "top 80%",
+      toggleActions: "play none none none"
+    }
+  });
+});
